049cce63c7ee58ceede8911e16e53de7cf546082
Tomas13d <tomas.demobio@gmail.com>
2023-08-13 18:45:07 -0300

    welcome and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import '../styles/globals.css'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return <Component {...pageProps} />
 }
